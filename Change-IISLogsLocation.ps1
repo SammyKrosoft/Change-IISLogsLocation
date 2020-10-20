22136bf21c2181a42ec5ba90e62bf994e17742dc
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2020-10-19 22:14:41 -0400

    Update Change-IISLogsLocation.ps1

--- Change-IISLogsLocation.ps1
+++ Change-IISLogsLocation.ps1
@@ -25,14 +25,19 @@ $DefaultLogLocation = "$($env:SystemDrive)\inetpub\logs\LogFile"
 Import-Module WebAdministration
 
 If ($SetIIS){
-    $Logdir = $NewLogLocation
-
-    ForEach ($Computer in $MyComputers){
+    [string]$Logdir = $NewLogLocation
+    ForEach ($Computer in $Computers){
         Write-Host "Changing IIS Logpath of server $Computer to $logdir" -BackgroundColor Yellow -ForegroundColor Blue
-         Invoke-command -ComputerName $Computer -ScriptBlock {Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name logfile.directory -value $logdir}
+        #Putting the whole expression into a string variable to invoke it later. Reason: the variable used $LogDir will be valid only inside the -ScriptBlock, and cannot be set before.
+        # Workaround is to save the whole command line into a string variable, and any variable within the string variable will be replaced with the value we set.
+        # example: $Logdir is set earlier in the script, and will be replaced by its value within the "$Expression" string variable.
+        $Expression = "Invoke-command -ComputerName $Computer -ScriptBlock {Set-WebConfigurationProperty `"/system.applicationHost/sites/siteDefaults`" -name logfile.directory -value $Logdir}"
+        Write-Host "Executing the following command:" -ForegroundColor Green
+        Write-Host $Expression
+        Invoke-Expression $Expression
     }
 } ElseIf ($GetIIS){
-    ForEach ($Computer in $MyComputers){
+    ForEach ($Computer in $Computers){
         Write-Host "Checking IIS Logpath of server $Computer" -BackgroundColor Yellow -ForegroundColor Blue
         Invoke-command -ComputerName $Computer -ScriptBlock {get-WebConfigurationProperty -Filter "/system.applicationHost/sites/siteDefaults" -Name LogFile.Directory.Value}
      }
