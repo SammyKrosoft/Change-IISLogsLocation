e39f6007b139f3bb58fe60f800cd021061d15923
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2020-10-19 23:47:14 -0400

    Update IISLogsLocation.ps1

--- IISLogsLocation.ps1
+++ IISLogsLocation.ps1
@@ -15,32 +15,27 @@ param(
 
 )
 
-
-# Putting default location (previous one just in case) in a variable
-$DefaultLogLocation = "$($env:SystemDrive)\inetpub\logs\LogFile"
-# Putting new target desired location in a variable
-# $NewLogLocation = "c:\IISLogsNewLocation" ==> Parameter
-
-#Just to simplify and for the sake of using variables, putting desired Log Location variable in $LogDir variable
-# If you want to change back to the default location, use $LogDir = $DefaultLogLocation
-
-# $MyComputers = "E2016-01", "E2016-02" ==> Parameter
-
 Import-Module WebAdministration
 
 If ($SetIIS){
-    If ($DefaultLocation){$Logdir = $DefaultLogLocation} Else {
-        [string]$Logdir = $NewLogLocation
-        }
     ForEach ($Computer in $Computers){
-        Write-Host "Changing IIS Logpath of server $Computer to $logdir" -BackgroundColor Yellow -ForegroundColor Blue
-        #Putting the whole expression into a string variable to invoke it later. Reason: the variable used $LogDir will be valid only inside the -ScriptBlock, and cannot be set before.
-        # Workaround is to save the whole command line into a string variable, and any variable within the string variable will be replaced with the value we set.
-        # example: $Logdir is set earlier in the script, and will be replaced by its value within the "$Expression" string variable.
-        $Expression = "Invoke-command -ComputerName $Computer -ScriptBlock {Set-WebConfigurationProperty `"/system.applicationHost/sites/siteDefaults`" -name logfile.directory -value $Logdir}"
+        
+       
+        If ($DefaultLocation){
+            Write-Host "Changing IIS Logpath of server $Computer to %SystemDrive%\inetpub\logs\logfile using remote computer environment variable" -BackgroundColor Yellow -ForegroundColor Blue
+            $Expression = "Invoke-command -ComputerName $Computer -ScriptBlock {`$DefaultIISFolder=`"`$([Environment]::GetEnvironmentVariable(`"SystemDrive`"))\inetpub\logs\LogFile`";Set-WebConfigurationProperty `"/system.applicationHost/sites/siteDefaults`" -name logfile.directory -value `$DefaultIISFolder}"
+        } Else {
+            $LogDir = $NewLogLocation
+            Write-Host "Changing IIS Logpath of server $Computer to $logdir" -BackgroundColor Yellow -ForegroundColor Blue
+            #Putting the whole expression into a string variable to invoke it later. Reason: the variable used $LogDir will be valid only inside the -ScriptBlock, and cannot be set before.
+            # Workaround is to save the whole command line into a string variable, and any variable within the string variable will be replaced with the value we want before invoke-command is even executed.
+            # example: $Logdir is set earlier in the script, and will be replaced by its value within the "$Expression" string variable.
+            $Expression = "Invoke-command -ComputerName $Computer -ScriptBlock {Set-WebConfigurationProperty `"/system.applicationHost/sites/siteDefaults`" -name logfile.directory -value $Logdir}"
+        }
         Write-Host "Executing the following command:" -ForegroundColor Green
         Write-Host $Expression
         Invoke-Expression $Expression
+        Write-Host "Double-check the remote servers IIS log location have been changed using $($MyInvocation.MyCommand.Name) -GetIIS -Computers $Computers" -ForegroundColor Cyan
     }
 } ElseIf ($GetIIS){
     ForEach ($Computer in $Computers){
